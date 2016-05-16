6ce217a599e8023603ff16091c5d18aa5fc8d889
unknown <dongtongtong@feinno.com>
2016-05-16 17:20:24 +0800

    公司提交

---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+/*
+* @Author: dongtongtong
+* @Date:   2016-05-12 20:43:58
+* @Last Modified by:   dongtongtong
+* @Last Modified time: 2016-05-12 20:53:38
+*/
+
+angular.module('config',[])
+.constant("global",{
+  'SERVEI_PATH':'127.0.0.1:3000'
+})
